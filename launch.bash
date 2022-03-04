8b43ce1dfbbc5528d8a1e6f0fa58bace3966d147
raucha <raucha@users.noreply.github.com>
2022-03-04 05:54:35 +0000

    change

--- launch.bash
+++ launch.bash
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-docker run -d -t --name vnc_test -p 6080:80 --shm-size=512m ubuntu:20.04
+docker run -p 6080:80 dorowu/ubuntu-desktop-lxde-vnc
+# docker run -it --name vnc_test -p 6080:80 --shm-size=512m ubuntu:20.04
